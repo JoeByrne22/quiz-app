1d65c5b27173eaf26f4baf536c2778ef90590a93
joe byrne <joebyrne@joes-MacBook-Pro.local>
2021-11-23 18:51:43 +0000

    data made

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log('running');
+quizData = [
+    {
+        question : "What colour is the sky?",
+        a : "red",
+        b : "blue",
+        c : "yellow",
+        d : "green",
+        answer : "b"
+    },
+    {
+        question : "How many legs does an octopus?",
+        a : "2",
+        b : "4",
+        c : "6",
+        d : "8",
+        answer : "d"
+    },
+    {
+        question : "When is Christmas?",
+        a : "25th Dec",
+        b : "1st Jan",
+        c : "19th June",
+        d : "7th Sept",
+        answer : "a"
+    }
+]
+
+console.log('quizData', quizData);
