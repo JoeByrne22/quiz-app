8dec2bdf25c3cdae89f4ae5faee11d3e2654bb71
joe byrne <joebyrne@joes-MacBook-Pro.local>
2021-11-30 14:56:53 +0000

    data and html done

--- script.js
+++ script.js
@@ -25,4 +25,26 @@ quizData = [
     }
 ]
 
-console.log('quizData', quizData);
+const quesitonEl = document.getElementById('question');
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const d_text = document.getElementById('d_text');
+
+let currentQuiz = 0;
+
+loadQuiz(); 
+
+function loadQuiz() {
+    const currentQuizData = quizData[currentQuiz];
+    quesitonEl.innerHTML = currentQuizData.question;
+
+    a_text.innerHTML = currentQuizData.a;
+    b_text.innerHTML = currentQuizData.b;
+    c_text.innerHTML = currentQuizData.c;
+    d_text.innerHTML = currentQuizData.d;
+
+
+    currentQuiz++;
+};
+
